5e3cf41f039e719aeed269b9d598aad8f04f3a91
Yuxin <76422684+Lyxxx2003@users.noreply.github.com>
2024-11-07 14:53:52 -0500

    Fetched from dev

---
+++ quizzybee/quizzybee/Views/Dashboard.swift
@@ -0,0 +1,18 @@
+//
+//  Dashboard.swift
+//  quizzybee
+//
+//  Created by 李雨欣 on 2024/11/7.
+//
+
+import SwiftUI
+
+struct Dashboard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Dashboard()
+}
